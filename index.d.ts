22491aca3ca20ff2dff16862175e641df9610225
NicolasNewman <nnewman316@gmail.com>
2023-03-09 10:27:25 -0600

    added typescript declarations

---
+++ index.d.ts
@@ -0,0 +1,2 @@
+export function convert(input: string): boolean | {kanji: false | string, hiragana: false | string, katakana: false | string, romaji: false | string}
+export function romanise(kana: string): string;
