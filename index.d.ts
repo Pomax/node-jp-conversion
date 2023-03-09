2dc343b24d8a5b11c9fdaa13058d30964fdcafbf
NicolasNewman <nnewman316@gmail.com>
2023-03-09 10:38:37 -0600

    switched from boolean to false for conversion return type

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,2 @@
-export function convert(input: string): boolean | {kanji: false | string, hiragana: false | string, katakana: false | string, romaji: false | string}
+export function convert(input: string): false | {kanji: false | string, hiragana: false | string, katakana: false | string, romaji: false | string}
 export function romanise(kana: string): string;
